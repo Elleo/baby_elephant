102928592f806b82d44dca6d082d77976bb6f9f1
Mike Sheldon <mike@mikeasoft.com>
2022-12-11 00:53:58 +0000

    Handle empty responses, load timeline at start, add internet permission

--- lib/timeline.dart
+++ lib/timeline.dart
@@ -24,6 +24,10 @@ class _TimelinePageState extends State<TimelinePage> {
   RefreshController refreshController =
       RefreshController(initialRefresh: false);
 
+  _TimelinePageState() {
+    updateTimeline(false);
+  }
+
   void onRefresh() async {
     updateTimeline(true);
   }
@@ -36,32 +40,41 @@ class _TimelinePageState extends State<TimelinePage> {
     } else {
       maxId = oldestStatus;
     }
-    var tlFuture = mastodon.v1.timelines
-        .lookupHomeTimeline(minStatusId: minId, maxStatusId: maxId);
-    tlFuture.then((timeline) => {
-          setState(() {
-            if (refresh && timeline.data.isNotEmpty) {
-              newestStatus = timeline.data[0].id;
-            }
-            timeline.data.forEach((status) {
-              if (status.content.isNotEmpty) {
-                if (refresh) {
-                  items.insert(0, status);
-                } else {
-                  items.add(status);
+    try {
+      var tlFuture = mastodon.v1.timelines
+          .lookupHomeTimeline(minStatusId: minId, maxStatusId: maxId);
+      tlFuture.then((timeline) => {
+            setState(() {
+              if ((refresh || newestStatus == null) &&
+                  timeline.data.isNotEmpty) {
+                newestStatus = timeline.data[0].id;
+              }
+              timeline.data.forEach((status) {
+                if (status.content.isNotEmpty) {
+                  if (refresh) {
+                    items.insert(0, status);
+                  } else {
+                    items.add(status);
+                  }
                 }
+                if (!refresh) {
+                  oldestStatus = status.id;
+                }
+              });
+              if (refresh) {
+                refreshController.refreshCompleted();
+              } else {
+                refreshController.loadComplete();
               }
-              if (!refresh) {
-                oldestStatus = status.id;
-              }
-            });
-            if (refresh) {
-              refreshController.refreshCompleted();
-            } else {
-              refreshController.loadComplete();
-            }
-          })
-        });
+            })
+          });
+    } on DataNotFoundException catch (_) {
+      if (refresh) {
+        refreshController.refreshCompleted();
+      } else {
+        refreshController.loadComplete();
+      }
+    }
   }
 
   void onLoading() async {
@@ -74,54 +87,59 @@ class _TimelinePageState extends State<TimelinePage> {
         body: SmartRefresher(
       enablePullDown: true,
       enablePullUp: true,
-      header: const WaterDropHeader(),
+      header: const MaterialClassicHeader(distance: 10),
       controller: refreshController,
       onRefresh: onRefresh,
       onLoading: onLoading,
-      child: ListView.builder(
-        itemBuilder: (c, i) => Card(
-            clipBehavior: Clip.hardEdge,
-            margin:
-                const EdgeInsets.only(left: 10, right: 10, top: 5, bottom: 5),
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-            child: Padding(
-                padding: const EdgeInsets.all(5),
-                child: Column(children: [
-                  Row(
-                    children: [
-                      ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
-                          child: Image(
-                              image: NetworkImage(items[i].account.avatar),
-                              width: 32,
-                              height: 32)),
-                      const SizedBox(width: 5),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                              items[i].account.displayName != ""
-                                  ? items[i].account.displayName
-                                  : items[i].account.username,
-                              textAlign: TextAlign.left,
-                              overflow: TextOverflow.clip,
-                              style:
-                                  const TextStyle(fontWeight: FontWeight.bold)),
-                          Text(
-                            "@${items[i].account.username}",
-                            style: const TextStyle(color: Colors.grey),
-                            textAlign: TextAlign.left,
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                  const SizedBox(height: 10),
-                  Center(child: HtmlWidget(items[i].content)),
-                ]))),
-        itemCount: items.length,
-      ),
+      child: items.isEmpty
+          ? const Center(
+              child: SizedBox(
+                  height: 64, width: 64, child: CircularProgressIndicator()))
+          : ListView.builder(
+              itemBuilder: (c, i) => Card(
+                  clipBehavior: Clip.hardEdge,
+                  margin: const EdgeInsets.only(
+                      left: 10, right: 10, top: 5, bottom: 5),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: Column(children: [
+                        Row(
+                          children: [
+                            ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: Image(
+                                    image:
+                                        NetworkImage(items[i].account.avatar),
+                                    width: 32,
+                                    height: 32)),
+                            const SizedBox(width: 5),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                    items[i].account.displayName != ""
+                                        ? items[i].account.displayName
+                                        : items[i].account.username,
+                                    textAlign: TextAlign.left,
+                                    overflow: TextOverflow.clip,
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.bold)),
+                                Text(
+                                  "@${items[i].account.username}",
+                                  style: const TextStyle(color: Colors.grey),
+                                  textAlign: TextAlign.left,
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                        const SizedBox(height: 10),
+                        Center(child: HtmlWidget(items[i].content)),
+                      ]))),
+              itemCount: items.length,
+            ),
     ));
   }
 }
