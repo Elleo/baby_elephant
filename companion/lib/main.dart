34f90b6cb6b7be0b84b3ac3b42c557b8cb1707df
Mike Sheldon <mike@mikeasoft.com>
2022-12-27 13:20:53 +0000

    Add licensing details

--- companion/lib/main.dart
+++ companion/lib/main.dart
@@ -1,3 +1,21 @@
+/*
+ * This file is part of Baby Elephant, a Mastodon client for smartwatches.
+ *
+ * Copyright (c) 2022 Mike Sheldon <mike@mikeasoft.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import 'package:flutter/material.dart';
 import 'auth.dart';
 
